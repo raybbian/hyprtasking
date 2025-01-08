fd74b7dea940e328869e2baf2b6e57e329cda541
Maticzpl <maticzpl06@gmail.com>
2025-01-08 14:38:21 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Hyprtasking";
+
+  inputs = {
+    hyprland.url = "github:hyprwm/Hyprland";
+    nixpkgs.follows = "hyprland/nixpkgs";
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs = { self, nixpkgs, hyprland, systems, ... }:
+    let
+      inherit (nixpkgs) lib;
+
+      forSystems = attrs: lib.genAttrs (import systems) (system:
+        attrs system nixpkgs.legacyPackages.${system}
+      );
+    in
+    {
+      packages = forSystems (system: pkgs: {
+        hyprtasking = let
+          hyprlandPkg = hyprland.packages.${system}.hyprland;
+        in
+        pkgs.gcc14Stdenv.mkDerivation {
+          pname = "hyprtasking";
+          version = "1.0";
+
+          src = ./.;
+
+          nativeBuildInputs = hyprlandPkg.nativeBuildInputs;
+          buildInputs = [hyprlandPkg] ++ hyprlandPkg.buildInputs;
+
+          meta = with lib; {
+            homepage = "https://github.com/raybbian/hyprtasking";
+            description = "Powerful workspace management plugin, packed with features ";
+            license = licenses.bsd3;
+            platforms = platforms.linux;
+          };
+        };
+
+        default = self.packages.${system}.hyprtasking;
+      });
+    };
+}
