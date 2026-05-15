844d93aee084de65361ef0870c52db97a5e72655
Yerlotic <Yerlotic@users.noreply.github.com>
2026-05-15 20:39:29 +0000

    Fix config caching

--- src/config.hpp
+++ src/config.hpp
@@ -13,36 +13,15 @@ using namespace Config::Values;
 namespace HTConfig {
 
 template<typename T>
-concept ptr_type = std::same_as<T, std::string>;
-
-template<ptr_type T>
 inline T value(std::string config) {
-    CConfigValue<T> val("plugin:hyprtasking:" + config);
-    // const auto var = (T const *)Config::mgr()->getConfigValue("plugin:hyprtasking:" + config).dataptr;
-    return *val;
-    // static std::unordered_map<std::string, T const*> cache;
-    //
-    // if (!cache.count(config))
-    //     cache[config] =
-    //         (T const*)HyprlandAPI::getConfigValue(PHANDLE, "plugin:hyprtasking:" + config)
-    //             ->getDataStaticPtr();
-    // return *cache[config];
-}
+    static std::unordered_map<std::string, CConfigValue<T>> cache;
 
-template<typename T>
-inline T value(std::string config)
-    requires(!ptr_type<T>)
-{
-    CConfigValue<T> val("plugin:hyprtasking:" + config);
-    // const auto var = (T const *)Config::mgr()->getConfigValue("plugin:hyprtasking:" + config).dataptr;
-    return *val;
-    // static std::unordered_map<std::string, T* const*> cache;
-    //
-    // if (!cache.count(config))
-    //     cache[config] =
-    //         (T* const*)HyprlandAPI::getConfigValue(PHANDLE, "plugin:hyprtasking:" + config)
-    //             ->getDataStaticPtr();
-    // return *cache[config];
+    if (!cache.count(config)) {
+        const CConfigValue<T> val("plugin:hyprtasking:" + config);
+        cache[config] = val;
+    }
+
+    return *cache[config];
 }
 
 } // namespace HTConfig
